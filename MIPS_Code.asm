1485cd8a9aac1d9d8d32c453eae10afd218e4fc1
therealdantec <dciarabellini@yahoo.com>
2024-06-08 23:39:24 -0700

    slight changes

--- MIPS_Code.asm
+++ MIPS_Code.asm
@@ -1,6 +1,8 @@
+#### Standard MIPS Assembly ####
+
 .data
-array: .word 5
-array1: .word 3
+array: .word 0:5
+array1: .word 0:3
 num1: .word 0
 num2: .word 0
 num3: .word 0
@@ -13,109 +15,135 @@ num6: .word 0
 main:
     li $t0, 12
     sw $t0, array($zero)
+    
     li $t0, 2500
     sw $t0, 4(array)
-    lw $t0, array($zero)
-    move $a0, $t0
+
+    lw $t0, 0(array)
     li $v0, 1
+    move $a0, $t0
     syscall
+
     lw $t0, 4(array)
-    move $a0, $t0
     li $v0, 1
+    move $a0, $t0
     syscall
-    lw $t0, array($zero)
-    addi $t0, $t0, 1000
-    sw $t0, array($zero)
+
+    lw $t0, 0(array)
+    li $t1, 1000
+    add $t0, $t0, $t1
+    sw $t0, 0(array)
+
     li $t0, 2
-    addi $t0, $t0, 0
+    add $t1, $t0, $zero
+    lw $t0, 4(array)
+    add $t0, $t0, $t1
+    sw $t0, 4(array)
+
+    lw $t0, 4(array)
     li $t1, 2
-    lw $t2, array($zero)
-    add $t0, $t0, $t2
-    sw $t0, array($zero)
+    add $t0, $t0, $t1
+    sw $t0, 4(array)
+
     li $t0, 1
-    addi $t0, $t0, 0
+    add $t1, $t0, $zero
+    lw $t0, 0(array)
+    add $t0, $t0, $t1
+    sw $t0, 0(array)
+
+    lw $t0, 0(array)
+    li $v0, 1
+    move $a0, $t0
+    syscall
+
+    lw $t0, 0(array)
     li $t1, 1
-    lw $t2, 4(array)
-    add $t0, $t0, $t2
+    add $t0, $t0, $t1
+    sw $t0, 0(array)
+
+    lw $t0, 0(array)
+    li $v0, 1
+    move $a0, $t0
+    syscall
+
+    lw $t0, 4(array)
+    li $t1, 500
+    sub $t0, $t0, $t1
     sw $t0, 4(array)
-    addi $t0, $zero, 0
-    move $a0, $t0
-    li $v0, 1
-    syscall
+    
     lw $t0, 4(array)
-    move $a0, $t0
     li $v0, 1
-    syscall
-    lw $t0, array($zero)
-    addi $t0, $t0, 1
-    sw $t0, array($zero)
-    addi $t0, $zero, 0
     move $a0, $t0
-    li $v0, 1
     syscall
+
     lw $t0, 4(array)
-    subi $t0, $t0, 500
-    sw $t0, 4(array)
-    lw $t0, 4(array)
-    move $a0, $t0
     li $v0, 1
-    syscall
-    lw $t0, 4(array)
     move $a0, $t0
-    li $v0, 1
     syscall
-    lw $t0, array($zero)
+
+    lw $t0, 0(array)
+    li $v0, 1
     move $a0, $t0
-    li $v0, 1
     syscall
+
+    jr $ra
 
 .globl main2
 main2:
     li $t0, 12
     sw $t0, array1($zero)
+    
     li $t0, 2500
     sw $t0, 4(array1)
-    lw $t0, array1($zero)
-    move $a0, $t0
+
+    lw $t0, 0(array1)
     li $v0, 1
-    syscall
-    lw $t0, 4(array1)
     move $a0, $t0
-    li $v0, 1
-    syscall
-    lw $t0, array1($zero)
-    div $t0, $t0, 1000
-    mflo $t0
-    sw $t0, array1($zero)
-    li $t0, 2
-    addi $t0, $t0, 0
-    lw $t2, array1($zero)
-    add $t0, $t0, $t2
-    sw $t0, array1($zero)
-    li $t0, 1
-    addi $t0, $t0, 0
-    lw $t2, 4(array1)
-    add $t0, $t0, $t2
-    sw $t0, 4(array1)
-    lw $t0, array1($zero)
-    subi $t0, $t0, 1
-    sw $t0, array1($zero)
-    lw $t0, array1($zero)
-    move $a0, $t0
-    li $v0, 1
-    syscall
-    lw $t0, array1($zero)
-    move $a0, $t0
-    li $v0, 1
-    syscall
-    lw $t0, 4(array1)
-    move $a0, $t0
-    li $v0, 1
     syscall
 
-.globl _start
-_start:
-    jal main
-    jal main2
-    li $v0, 10
+    lw $t0, 4(array1)
+    li $v0, 1
+    move $a0, $t0
     syscall
+
+    lw $t0, 0(array1)
+    li $t1, 1000
+    div $t0, $t0, $t1
+
+    li $t1, 2
+    add $t2, $t1, $t0
+    sw $t2, 4(array1)
+
+    lw $t0, 4(array1)
+    li $t1, 2
+    mul $t0, $t0, $t1
+    sw $t0, 4(array1)
+
+    li $t0, 1
+    li $t1, 1
+    add $t2, $t1, $zero
+    lw $t0, 4(array1)
+    add $t0, $t0, $t2
+    sw $t0, 4(array1)
+
+    lw $t0, 0(array1)
+    li $t1, 1
+    sub $t0, $t0, $t1
+    sw $t0, 0(array1)
+
+    lw $t0, 0(array1)
+    li $v0, 1
+    move $a0, $t0
+    syscall
+
+    lw $t0, 0(array1)
+    li $v0, 1
+    move $a0, $t0
+    syscall
+
+    lw $t0, 4(array1)
+    li $v0, 1
+    move $a0, $t0
+    syscall
+
+    jr $ra
